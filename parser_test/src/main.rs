078c641ff412dfdc5093e3d2896c03335f019962
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2024-08-03 05:03:45 +0200

    ff

--- parser_test/src/main.rs
+++ parser_test/src/main.rs
@@ -1,27 +1,28 @@
 use core::str;
 
 use pom::parser::*;
+use utils::{into_string, passive_tag, space};
 
 fn main() {
-    let input = b"abcde";
-    let parser = sym(b'a') * none_of(b"AB") - sym(b'c') + seq(b"de");
-    let output = parser.parse(input);
-    assert_eq!(output, Ok((b'b', vec![b'd', b'e'].as_slice())));
+    // let input = b"abcde";
+    // let parser = sym(b'a') * none_of(b"AB") - sym(b'c') + seq(b"de");
+    // let output = parser.parse(input);
+    // assert_eq!(output, Ok((b'b', vec![b'd', b'e'].as_slice())));
 }
 
-fn string() -> Parser<'static, u8, String> {
-    let special_char = sym(b'\\')
-        | sym(b'/')
-        | sym(b'"')
-        | sym(b'b').map(|_| b'\x08')
-        | sym(b'f').map(|_| b'\x0C')
-        | sym(b'n').map(|_| b'\n')
-        | sym(b'r').map(|_| b'\r')
-        | sym(b't').map(|_| b'\t');
-    let escape_sequence = sym(b'\\') * special_char;
-    let string = sym(b'"') * (none_of(b"\\\"") | escape_sequence).repeat(0..) - sym(b'"');
-    string.convert(String::from_utf8)
-}
+// fn string() -> Parser<'static, u8, String> {
+//     let special_char = sym(b'\\')
+//         | sym(b'/')
+//         | sym(b'"')
+//         | sym(b'b').map(|_| b'\x08')
+//         | sym(b'f').map(|_| b'\x0C')
+//         | sym(b'n').map(|_| b'\n')
+//         | sym(b'r').map(|_| b'\r')
+//         | sym(b't').map(|_| b'\t');
+//     let escape_sequence = sym(b'\\') * special_char;
+//     let string = sym(b'"') * (none_of(b"\\\"") | escape_sequence).repeat(0..) - sym(b'"');
+//     string.convert(String::from_utf8)
+// }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 struct ReleaseTitle {
@@ -29,64 +30,93 @@ struct ReleaseTitle {
     pub title: Option<String>,
 }
 
-fn release_title(src: &str) -> anyhow::Result<ReleaseTitle> {
-    let version =
-        sym(b'#').repeat(2) * sym(b' ') * sym(b'[') * none_of(b"\n]").repeat(1..) - sym(b']');
+mod utils {
+    use pom::{parser::*};
 
-    let title = sym(b' ') * sym(b'-') * sym(b' ') * none_of(b"\n]").repeat(1..);
+    pub fn into_string(v: Vec<char>) -> String {
+        let str = v.into_iter().collect::<String>();
+        let str = str.trim();
+        str.to_owned()
+    }
+
+ 
+
+    pub fn space<'a>() -> Parser<'a, char, ()> {
+        one_of(" \t\r\n").repeat(0..).discard()
+    }
+}
+
+fn header<'a>() -> Parser<'a, char, Option<String>> {
+    (any() - (!tag("\n## ["))).repeat(0..).convert(|header| {
+        let header = into_string(header);
+
+        if header.is_empty() {
+            Ok::<_, ()>(None)
+        } else {
+            Ok(Some(header))
+        }
+    })
+}
+
+
+
+fn release_title<'a>() -> Parser<'a, char, ReleaseTitle> {
+    let version =
+        space() * sym('#').repeat(2) * sym(' ') * sym('[') * none_of("\n]").repeat(1..) - sym(']');
+
+    let title = sym(' ') * sym('-') * sym(' ') * none_of("\n]").repeat(1..);
 
     let parser = version + title.opt();
 
-    let parser = parser.convert(|(version, title)| {
-        let res = unsafe {
-            ReleaseTitle {
-                version: String::from_utf8_unchecked(version),
-                title: title.map(|title| String::from_utf8_unchecked(title)),
-            }
+    parser.convert(|(version, title)| {
+        let res = ReleaseTitle {
+            version: into_string(version),
+            title: title.map(|title| into_string(title)),
         };
 
         Ok::<ReleaseTitle, ()>(res)
-    });
-
-    let res = parser.parse(src.as_bytes())?;
-
-    Ok(res)
+    })
 }
 
 #[test]
 fn t() {
-    let input = "## [2024.7] - 2024-07-24";
+    let input = r#"
+hello
+la miff
 
-    let res = release_title(input);
+## [2024.7] - 2024-07-24
+
+"#;
+
+    let input = input.chars();
+
+    let input = input.collect::<Vec<_>>();
+
+    let res = header();
+
+    let res = res.parse(&input).unwrap();
 
     dbg!(&res);
 }
 
-// #[derive(Debug, Clone, PartialEq, Eq)]
-// struct ReleaseTitle {
-//     pub version: String,
-// }
+#[test]
+fn t2() {
+    let input = r#"
+hello
+la miff
 
-// fn release_title(src: &str) -> Option<ReleaseTitle> {
-//     let parser =
-//         sym(b'#').repeat(2) * sym(b' ') * sym(b'[') * none_of(b"\n]").repeat(1..) - sym(b']');
 
-//     let parser =
-//         parser.convert(|bytes| unsafe { Ok::<String, ()>(String::from_utf8_unchecked(bytes)) });
+## [2024.7] - 2024-07-24
 
-//     // called `Result::unwrap()` on an `Err` value: Incomplete
-//     let version = parser.parse(src.as_bytes()).unwrap();
+"#;
 
-//     Some(ReleaseTitle {
-//         version,
-//     })
-// }
+    let input = input.chars();
 
-// #[test]
-// fn test() {
-//     let input = "## [2024]";
+    let input = input.collect::<Vec<_>>();
 
-//     let res = release_title(input);
+    let res = header() + release_title();
 
-//     dbg!(&res);
-// }
+    let res = res.parse(&input).unwrap();
+
+    dbg!(&res);
+}
