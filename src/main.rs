d265323477a0a4b45fc9619da465924891a7a6a3
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2024-08-22 23:42:40 +0200

    hotfix

--- src/main.rs
+++ src/main.rs
@@ -8,3 +8,28 @@ fn main() -> anyhow::Result<()> {
 
     run(cli)
 }
+
+mod gen {
+
+    use std::{fs::OpenOptions, io::Write};
+
+    use changelog_gen::config::Cli;
+
+    #[ignore = ""]
+    #[test]
+    fn gen() -> anyhow::Result<()> {
+        let doc = clap_markdown::help_markdown::<Cli>();
+
+        let mut file = OpenOptions::new()
+            .create(true)
+            .truncate(true)
+            .write(true)
+            .open("res/API_REFERENCE.md")?;
+
+        file.write_all(doc.as_bytes())?;
+
+        println!("Documentation successfully written");
+
+        Ok(())
+    }
+}
